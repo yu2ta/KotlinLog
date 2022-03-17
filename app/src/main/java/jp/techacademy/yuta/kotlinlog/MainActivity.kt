0beaaf7a2e9ccf5635955077242760bb4f6a5f5e
yu2ta <yuta_matsuo@r.recruit.co.jp>
2022-03-17 17:49:50 +0900

    課題１提出

--- app/src/main/java/jp/techacademy/yuta/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuta/kotlinlog/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         var satou = Human("佐藤", 38, "釣り")
         var tanaka = Human("田中", 34, "撮り鉄を撮る特殊な趣味")
 
